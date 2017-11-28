97005427f2ae0ee142842df714b8c0b3667fa0ba
yajamon <yajamon.tatsuki@gmail.com>
2017-11-28 15:25:13 +0900

    Add new script

---
+++ script/new.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+readonly REPOROOT=$(dirname $(realpath $0/../));
+
+section=""
+while [ -z $section ]; do
+    echo -n "section name :" >&2
+    read -r section _trush
+done
+
+cd $REPOROOT
+cargo new study_$section --bin
+git checkout -b $section
+
+git add $REPOROOT/study_$section
