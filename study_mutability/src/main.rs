bfd410e75c667b5eab47c7c217b331a6c1bb410a
yajamon <yajamon.tatsuki@gmail.com>
2017-11-28 16:17:27 +0900

    基本的なところ

--- study_mutability/src/main.rs
+++ study_mutability/src/main.rs
@@ -1,3 +1,20 @@
+use std::sync::Arc;
+
 fn main() {
-    println!("Hello, world!");
+    // Error
+    // let x = 5;
+    // x = 6;
+
+    // Valid
+    let mut x = 5;
+    x = 6;
+    // 変数束縛が何を指すか、が変更できる
+
+    // 束縛が指す先を変更する場合は、ミュータブル参照を使う
+    let y = &mut x;
+    *y = 8;
+
+    // mut はパターンの一部になれる
+    let (mut first, second) = (1, 2);
+
 }
