9c950e7ff678d16dff210e0601a0ee5ed49f0dfa
yajamon <yajamon.tatsuki@gmail.com>
2017-12-30 21:48:41 +0900

    構造体のミュータビリティについて追記

--- study_structs/src/main.rs
+++ study_structs/src/main.rs
@@ -10,5 +10,15 @@ fn main() {
     // let origin_y = 0;
     let origin = Point {x: 0, y: 0};
 
+    // 束縛にミュータビリティを付与することで値を変更できる
+    let mut point = Point { x: 0, y: 0};
+    point.x = 5;
+
+    // 新しい束縛によってここから変更できなくなる
+    let point = point;
+
+    // // 変更できないのでこれはエラー
+    // point.y = 6;
+
     println!("The origin is at ({}, {})", origin.x, origin.y);
 }
