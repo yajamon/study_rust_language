4029d021429a89f64d492ab19dab8165b99d5613
yajamon <yajamon.tatsuki@gmail.com>
2018-01-19 20:13:10 +0900

    builderマクロを更新
    初期値用の構文を追加
    newとbuildを追加

--- sandbox_macros/src/main.rs
+++ sandbox_macros/src/main.rs
@@ -5,13 +5,27 @@ macro_rules! m_vec {
 
 macro_rules! builder {
     ($src_name:ident => $dest_name:ident {
-        $( $attr_name:ident : $attr_type:ty ),*
+        $( $attr_name:ident : $attr_type:ty = $attr_default:expr ),*
     }) => {
         struct $dest_name {
             $( $attr_name : $attr_type ),*
         }
+
         struct $src_name {
             $( $attr_name : $attr_type ),*
+        }
+        impl $src_name {
+            pub fn new() -> $src_name {
+                $src_name {
+                    $( $attr_name : $attr_default ),*
+                }
+            }
+
+            pub fn build(&self) -> $dest_name {
+                $dest_name {
+                    $($attr_name : self.$attr_name.clone()),*
+                }
+            }
         }
     }
 }
@@ -21,14 +35,10 @@ fn main() {
     m_vec!(hello);
 
     builder!(UserBuilder => User {
-        id: i32,
-        name: String
+        id: i32 = 0,
+        name: String = "hoge".to_string()
     });
 
-    let user = User {
-        id: 1,
-        name: "Pater".to_string(),
-    };
-
+    let user = UserBuilder::new().build();
     println!("id: {}, name: {}", user.id, user.name);
 }
