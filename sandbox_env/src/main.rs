7ed3d626a83433af91ba03186f2b4e0fc0aae4ee
yajamon <yajamon.tatsuki@gmail.com>
2018-03-01 21:13:03 +0900

    env標準ライブラリで引数や環境変数の取得ができる

--- sandbox_env/src/main.rs
+++ sandbox_env/src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // env::varは引数のkeyを元に環境変数を模索する
+    let key = "HOME";
+    match env::var(key) {
+        Ok(val) => println!("{}: {:?}", key, val),
+        Err(e) => println!("取得できませんでした {}: {}", key, e),
+    }
+
+    // 引数を振り回すこともできる
+    println!("引数の一覧");
+    for argument in env::args() {
+        println!("{}", argument);
+    }
 }
