59226b5e85bd758ac53c5b72f165ab7bb5856b13
yajamon <yajamon.tatsuki@gmail.com>
2018-03-08 00:10:30 +0900

    Configバージョンごとに構造体を作成した

--- sandbox_from_migration/src/main.rs
+++ sandbox_from_migration/src/main.rs
@@ -1,29 +1,32 @@
 fn main() {
     let c = get_config(1);
     println!("config: {:?}", c);
-    let c2: Configs::V2 = get_config(2);
+    let c2: ConfigList = get_config(2);
+    println!("config: {:?}", c);
 }
 
 fn get_config(version: u8) -> ConfigList {
     match version {
-        1 => ConfigList::V01 { version: version },
-        2 => ConfigList::V02 {
+        1 => ConfigList::V01(ConfigV01 { version: version }),
+        2 => ConfigList::V02(ConfigV02 {
             version: version,
             a: "AAA".to_string(),
-        },
+        }),
         _ => panic!("undefined version: {}", version),
     }
 }
 
 #[derive(Debug)]
 enum ConfigList {
-    V01 { version: u8 },
-    V02 { version: u8, a: String },
+    V01(ConfigV01),
+    V02(ConfigV02),
 }
 
+#[derive(Debug)]
 struct ConfigV01 {
     version: u8,
 }
+#[derive(Debug)]
 struct ConfigV02 {
     version: u8,
     a: String,
