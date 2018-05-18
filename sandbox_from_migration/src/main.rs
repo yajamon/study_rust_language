6fc85939f52073b307c83be756fce12999f4031e
yajamon <yajamon.tatsuki@gmail.com>
2018-05-18 21:14:24 +0900

    実装した

--- sandbox_from_migration/src/main.rs
+++ sandbox_from_migration/src/main.rs
@@ -1,30 +1,61 @@
 fn main() {
     let c = get_config(1);
     println!("config: {:?}", c);
-    let c2: Configs::V2 = get_config(2);
+    let c2: Config = migrate(c);
+    println!("config: {:?}", c2);
 }
+
+type Config = ConfigV02;
 
 fn get_config(version: u8) -> ConfigList {
     match version {
-        1 => ConfigList::V01 { version: version },
-        2 => ConfigList::V02 {
+        1 => ConfigList::V01(ConfigV01 {
             version: version,
             a: "AAA".to_string(),
-        },
+        }),
+        2 => ConfigList::V02(ConfigV02 {
+            version: version,
+            a: "ABABAB".to_string(),
+            b: "CDCDCD".to_string(),
+        }),
         _ => panic!("undefined version: {}", version),
+    }
+}
+
+fn migrate(config: ConfigList) -> Config {
+    let mut config = config;
+    loop {
+        config = match config {
+            ConfigList::V01(c) => ConfigList::V02(c.into()),
+            ConfigList::V02(c) => return c,
+        }
     }
 }
 
 #[derive(Debug)]
 enum ConfigList {
-    V01 { version: u8 },
-    V02 { version: u8, a: String },
+    V01(ConfigV01),
+    V02(ConfigV02),
 }
 
+#[derive(Debug)]
 struct ConfigV01 {
     version: u8,
+    a: String,
 }
+#[derive(Debug)]
 struct ConfigV02 {
     version: u8,
     a: String,
+    b: String,
+}
+
+impl From<ConfigV01> for ConfigV02 {
+    fn from(c: ConfigV01) -> ConfigV02 {
+        ConfigV02 {
+            version: 2,
+            a: c.a.clone(),
+            b: "migration".to_string(),
+        }
+    }
 }
