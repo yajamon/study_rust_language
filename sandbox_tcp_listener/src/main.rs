5c165d1876027c25ff0ae773b70b8b2a1288c707
yajamon <yajamon.tatsuki@gmail.com>
2018-06-06 17:49:27 +0900

    TcpListerをbindしてみた

--- sandbox_tcp_listener/src/main.rs
+++ sandbox_tcp_listener/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{TcpListener, TcpStream};
+use std::io;
+
+fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:80").unwrap();
+
+    Ok(())
 }
