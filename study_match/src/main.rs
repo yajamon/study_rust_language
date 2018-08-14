9e371b850001fce9a627df63757a9b119dceb230
yajamon <yajamon.tatsuki@gmail.com>
2018-08-14 18:40:29 +0900

    列挙型に対するmatchについて記載

--- study_match/src/main.rs
+++ study_match/src/main.rs
@@ -21,4 +21,40 @@ fn main() {
         _ => "something else",
     };
     println!("{}", number);
+
+    process_mesasge(Message::Quit);
+    process_mesasge(Message::ChangeColor(0, 127, 255));
+    process_mesasge(Message::Move { x: 2, y: 5 });
+    process_mesasge(Message::Write("text".to_string()));
+}
+
+// 列挙型に対するmatchは網羅性検査と合わさって非常に重要
+enum Message {
+    Quit,
+    ChangeColor(i32, i32, i32),
+    Move { x: i32, y: i32 },
+    Write(String),
+}
+
+fn quit() {
+    println!("quit");
+}
+fn change_color(r: i32, g: i32, b: i32) {
+    println!("r:{} g:{} b:{}", r, g, b);
+}
+fn move_cursor(x: i32, y: i32) {
+    println!("move to x:{} y:{}", x, y);
+}
+
+fn process_mesasge(msg: Message) {
+    // matchは網羅性検査を行う
+    // 列挙型のすべてのバリアントに対してマッチするパターンがあることを要求する
+    // 一つでもマッチしないバリアントを残している場合、_を使わなければ
+    // コンパイルエラーが発生する
+    match msg {
+        Message::Quit => quit(),
+        Message::ChangeColor(r, g, b) => change_color(r, g, b),
+        Message::Move { x, y } => move_cursor(x, y),
+        Message::Write(s) => println!("{}", s),
+    }
 }
