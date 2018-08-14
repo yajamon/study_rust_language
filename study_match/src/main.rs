f241ccbeade2bf06aef50f3b9f1add55db4313ad
yajamon <yajamon.tatsuki@gmail.com>
2018-08-14 18:09:02 +0900

    matchの基本的な構文について記載

--- study_match/src/main.rs
+++ study_match/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    // 与えられた式が一致するパターンの式を実行する。
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        4 => println!("four"),
+        5 => println!("five"),
+        // matchは網羅性検査を行う。アンダースコアはその他の値すべてを受け入れる。
+        _ => println!("something else"),
+    }
 }
