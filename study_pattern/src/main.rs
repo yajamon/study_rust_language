a870e08faf27d3d8ac03a6c4b52e3c9ea7c05692
yajamon <yajamon.tatsuki@gmail.com>
2018-10-11 16:53:05 +0900

    パターンについて、簡単な復習内容を追加

--- study_pattern/src/main.rs
+++ study_pattern/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    // パターンはRustにおいて極めて一般的
+    let x = 1;
+
+    // リテラルに対しては直接マッチさせられる
+    // _ は任意のケースとして振る舞う
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
 }
