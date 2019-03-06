32913595522c77413de402ca343700cdc6c6a76f
yajamon <yajamon.tatsuki@gmail.com>
2019-03-06 19:03:59 +0900

    関連関数の説明
    用途のひとつとして、structの初期化をnew関数に置き換えたパターンを書いた
    これだけではただ一行で書いただけで、引数の順番という制約も生まれるため
    メリットは薄い。

--- study_method_syntax/src/main.rs
+++ study_method_syntax/src/main.rs
@@ -26,14 +26,27 @@ impl Circle {
     }
 }
 
+impl Circle {
+    // selfを引数に取らない関数も定義できる。これを関連関数と呼ぶ。
+    fn new(x: f64, y: f64, radius: f64) -> Circle {
+        Circle {
+            x: x,
+            y: y,
+            radius: radius,
+        }
+    }
+}
+
 fn main() {
     // Rustは `impl` キーワードによって「メソッド呼び出し構文」の機能を提供している
 
-    let c = Circle {
-        x: 0.0,
-        y: 0.0,
-        radius: 2.0,
-    };
+    // let c = Circle {
+    //     x: 0.0,
+    //     y: 0.0,
+    //     radius: 2.0,
+    // };
+    // 関連関数は Struct::function() という構文で呼び出される
+    let c = Circle::new(0.0, 0.0, 2.0);
     println!("{}", c.area());
     let d = c.grow(2.0).area();
     println!("{}", d);
