25d8ed7ee1b8b5858cab587d76a2894bb7e35fe7
yajamon <yajamon.tatsuki@gmail.com>
2019-03-06 17:15:30 +0900

    基本的な構文を学習

--- study_method_syntax/src/main.rs
+++ study_method_syntax/src/main.rs
@@ -1,3 +1,21 @@
+struct Circle {
+    x: f64,
+    y: f64,
+    radius: f64,
+}
+
+impl Circle {
+    fn area(&self) -> f64 {
+        std::f64::consts::PI * (self.radius * self.radius)
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    // Rustは `impl` キーワードによって「メソッド呼び出し構文」の機能を提供している
+
+    let c = Circle {
+        x: 0.0,
+        y: 0.0,
+        radius: 2.0,
+    };
+    println!("{}", c.area());
 }
